bc985c1761fbc011f8264d8ed218e1a8e09ed721
yiwjia <yiwjia@gmail.com>
2019-03-13 13:00:40 +0100

    test
    det er en test

--- game.js
+++ game.js
@@ -12,4 +12,6 @@ function victory({name}) { // return winner name
 function update({name}, total) { // update who is the active player and total
 
 }
+
+// tests
 // game.take(n, update) button event
